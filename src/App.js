dff0efbfb9fa251e8faab843417244a110d62ff4
TinhNT852 <127505668+TinhNT852@users.noreply.github.com>
2025-02-20 10:33:15 +0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import DashBoard from "./components/dashboard/DashBoard";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AccountTable from "./components/account/AccountTable";
-import ListAcc from "./components/account/control/ListAcc";
 import Authorization from "./components/account/control/Authorization";
+import ListAcc from "./components/account/control/ListAcc";
+import DashBoard from "./components/dashboard/DashBoard";
 import HomePage from "./components/Home/HomePage";
+import ProductsPage from "./components/Product/ProductPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashBoard />}>
+
           <Route path="/home" element={<HomePage />} />
-          <Route path="users" element={<AccountTable />}> 
+
+          <Route path="users" element={<AccountTable />}>
             <Route index element={<Navigate to ="/users/list" replace/>}/>
             <Route path="/users/list" element={<ListAcc />} />
             <Route path="/users/author" element={<Authorization S/>}/>
           </Route>
-          {/* <Route path="products" element={<ProductsPage />} />
-          <Route path="customers" element={<CustomersPage />} />
+
+          <Route path="/products" element={<ProductsPage />} />
+          
+          {/* <Route path="customers" element={<CustomersPage />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="notification" element={<NotificationPage />} /> */}
         </Route>
